bc370f4e96e14ead2ab78fa7b03f5bda2c68b0e3
Aditya Singh <aditya125siingh@gmail.com>
2024-10-21 00:20:51 +0530

    commit currency conveter

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      red: "#ff0000",
+      c_white: '#fff',
+      blue: "#0000ff",
+      black: "#000"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
